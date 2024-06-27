b02b17a87bf1585f114f2485dee3d21085ca95bd
István Kurucz <kurucz.isti63@gmail.com>
2024-06-27 10:54:00 +0200

    Some assets files added.

---
+++ src/assets/leagues/leagues.js
@@ -0,0 +1,11 @@
+export const leagues = new Map([
+	[
+		4694,
+		{
+			leagueName: "European Chamionship 2024",
+			leagueShortcut: "em2024",
+			leagueSeason: "2024",
+			sport: null,
+		},
+	],
+]);
---
+++ src/assets/teams/teamsEuro2024.js
@@ -0,0 +1,223 @@
+// Set for the teams playing in Euro 2024
+/* Structure of the entries:
+	- key: ID of the team
+	- value: { teamName, shortName, teamIconUrl, teamGroupName } */
+
+export const teams = new Map([
+	[
+		6169,
+		{
+			teamName: "Albania",
+			shortName: "ALB",
+			teamIconUrl: "https://img.uefa.com/imgml/flags/140x140/ALB.png",
+			teamGroupName: null,
+		},
+	],
+	[
+		2673,
+		{
+			teamName: "Belgium",
+			shortName: "BEL",
+			teamIconUrl: "https://img.uefa.com/imgml/flags/50x50/BEL.png",
+			teamGroupName: null,
+		},
+	],
+	[
+		758,
+		{
+			teamName: "Denmark",
+			shortName: "DEN",
+			teamIconUrl: "https://img.uefa.com/imgml/flags/140x140/DEN.png",
+			teamGroupName: null,
+		},
+	],
+	[
+		139,
+		{
+			teamName: "Germany",
+			shortName: "GER",
+			teamIconUrl: "https://img.uefa.com/imgml/flags/140x140/FRG.png",
+			teamGroupName: null,
+		},
+	],
+	[
+		755,
+		{
+			teamName: "England",
+			shortName: "ENG",
+			teamIconUrl: "https://img.uefa.com/imgml/flags/50x50/ENG.png",
+			teamGroupName: null,
+		},
+	],
+	[
+		144,
+		{
+			teamName: "France",
+			shortName: "FRA",
+			teamIconUrl: "https://img.uefa.com/imgml/flags/140x140/FRA.png",
+			teamGroupName: null,
+		},
+	],
+	[
+		6239,
+		{
+			teamName: "Georgia",
+			shortName: "GEO",
+			teamIconUrl: "https://img.uefa.com/imgml/flags/140x140/GEO.png",
+			teamGroupName: null,
+		},
+	],
+	[
+		145,
+		{
+			teamName: "Italy",
+			shortName: "ITA",
+			teamIconUrl: "https://img.uefa.com/imgml/flags/140x140/ITA.png",
+			teamGroupName: null,
+		},
+	],
+	[
+		146,
+		{
+			teamName: "Croatia",
+			shortName: "CRO",
+			teamIconUrl: "https://img.uefa.com/imgml/flags/140x140/CRO.png",
+			teamGroupName: null,
+		},
+	],
+	[
+		147,
+		{
+			teamName: "Netherlands",
+			shortName: "NED",
+			teamIconUrl: "https://img.uefa.com/imgml/flags/140x140/NED.png",
+			teamGroupName: null,
+		},
+	],
+	[
+		37,
+		{
+			teamName: "Austria",
+			shortName: "AUT",
+			teamIconUrl: "https://img.uefa.com/imgml/flags/140x140/AUT.png",
+			teamGroupName: null,
+		},
+	],
+	[
+		1410,
+		{
+			teamName: "Poland",
+			shortName: "POL",
+			teamIconUrl: "https://img.uefa.com/imgml/flags/140x140/POL.png",
+			teamGroupName: null,
+		},
+	],
+	[
+		149,
+		{
+			teamName: "Portugal",
+			shortName: "POR",
+			teamIconUrl: "https://img.uefa.com/imgml/flags/140x140/POR.png",
+			teamGroupName: null,
+		},
+	],
+	[
+		143,
+		{
+			teamName: "Romania",
+			shortName: "ROU",
+			teamIconUrl: "https://img.uefa.com/imgml/flags/140x140/ROU.png",
+			teamGroupName: null,
+		},
+	],
+	[
+		5271,
+		{
+			teamName: "Scotland",
+			shortName: "SCO",
+			teamIconUrl: "https://img.uefa.com/imgml/flags/140x140/SCO.png",
+			teamGroupName: null,
+		},
+	],
+	[
+		38,
+		{
+			teamName: "Switzerland",
+			shortName: "SUI",
+			teamIconUrl: "https://img.uefa.com/imgml/flags/140x140/SUI.png",
+			teamGroupName: null,
+		},
+	],
+	[
+		1404,
+		{
+			teamName: "Serbia",
+			shortName: "SRB",
+			teamIconUrl: "https://img.uefa.com/imgml/flags/140x140/SRB.png",
+			teamGroupName: null,
+		},
+	],
+	[
+		763,
+		{
+			teamName: "Slowakei",
+			shortName: "SVK",
+			teamIconUrl: "https://img.uefa.com/imgml/flags/140x140/SVK.png",
+			teamGroupName: null,
+		},
+	],
+	[
+		848,
+		{
+			teamName: "Slovenia",
+			shortName: "SLO",
+			teamIconUrl: "https://img.uefa.com/imgml/flags/140x140/SVN.png",
+			teamGroupName: null,
+		},
+	],
+	[
+		170,
+		{
+			teamName: "Spain",
+			shortName: "SPA",
+			teamIconUrl: "https://img.uefa.com/imgml/flags/140x140/ESP.png",
+			teamGroupName: null,
+		},
+	],
+	[
+		141,
+		{
+			teamName: "Czechia",
+			shortName: "CZE",
+			teamIconUrl: "https://img.uefa.com/imgml/flags/140x140/CZE.png",
+			teamGroupName: null,
+		},
+	],
+	[
+		153,
+		{
+			teamName: "Turkey",
+			shortName: "TUR",
+			teamIconUrl: "https://img.uefa.com/imgml/flags/140x140/TUR.png",
+			teamGroupName: null,
+		},
+	],
+	[
+		3204,
+		{
+			teamName: "Ukraine",
+			shortName: "UKR",
+			teamIconUrl: "https://img.uefa.com/imgml/flags/140x140/UKR.png",
+			teamGroupName: null,
+		},
+	],
+	[
+		1395,
+		{
+			teamName: "Hungary",
+			shortName: "HUN",
+			teamIconUrl: "https://img.uefa.com/imgml/flags/50x50/HUN.png",
+			teamGroupName: null,
+		},
+	],
+]);
